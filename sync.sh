90233d12375de062aefc3e93dcc73b8dfc7a389e
vlimki <vlimki@proton.me>
2025-08-17 13:49:39 +0300

    updates

---
+++ sync.sh
@@ -0,0 +1,2 @@
+rm portage/* -rf
+cp /etc/portage/package.env /etc/portage/env/ /etc/portage/package.use/ /etc/portage/package.mask/ /etc/portage/make.conf /etc/portage/savedconfig/ /var/lib/portage/world portage/ -r
